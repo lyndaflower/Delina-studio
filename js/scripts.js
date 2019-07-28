992cc0d981c0935c700bbfc58d8a2f4d32ccdf04
lyndaflower <umurerwalynda@gmail.com>
2019-07-28 17:09:12 +0200

    add another change

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,40 @@ $(document).ready(function(){
     $("#clickable").click(function(){
         $("#clickable").show();
         $("#design").toggle();
-    });
-    $("#design").click(function(){
-        $("#design").show();
         $("#clickable").toggle();
     });
-    $("#develop").click(function(){
-        $("#clickable2").show();
-        $("").toggle();
+    $("#design").click(function(){
+        $("#clickable").show();
+        $("#design").toggle();
     });
     $("#clickable2").click(function(){
+        $("#clickable2").show();
+        $("#develop").toggle();
+        $("#clickable2").toggle();
+    });
+    $("#develop").click(function(){
         $("#clickable2").show();
         $("#develop").toggle();
     });
     $("#clickable3").click(function(){
         $("#clickable3").show();
-        $("#product").hide();
+        $("#product").toggle();
+        $("#clickable3").toggle()
     });
-    $("#clickable3").click(function(){
+    $("#product").click(function(){
         $("#clickable3").show();
         $("#product").hide();
     });
+    $("#send").click(function(event){
+        var name = $("#name").val();
+        var Email = $("#Email").val();
+        var message = $("#message").val();
+        if((name !== " ") || (Email !== " ") || (message !== " ")) {
+            alert(name + " " +"thank you, we have receive your message");
+        }
+        else {
+            alert("sorry!Fill the form");
+        }
+        event.preventDefault();
+        });
 });
